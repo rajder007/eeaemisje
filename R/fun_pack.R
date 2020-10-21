b1e8ae27c0cd0b5c3125d696d67b3f62e9833906
krzys <krzysztof.zaczyk7@gmail.com>
2020-10-21 12:18:41 +0200

    Initial commit

---
+++ R/fun_pack.R
@@ -0,0 +1,30 @@
+obl_emis <- function(dane = input,
+                     kategoria = "Passenger Cars",
+                     #paliwo = "Petrol",
+                     #segment = "Mini",
+                     euro = "Euro 5",
+                     #technologia = "GDI",
+                     mode = "",
+                     substancja = c("EC", "CO")) {
+
+  # Zabezpieczenia argumentów funkcji i danych wejscioweych.
+
+  out <- wskazniki %>%
+    filter(Category %in% kategoria) %>%
+    filter(Fuel %in% paliwo) %>%
+    filter(Euro.Standard %in% euro) %>%
+    filter(Technology %in% technologia) %>%
+    filter(Pollutant %in% substancja) %>%
+    filter(Mode %in% mode)
+
+  out <- inner_join(x = out, y = input, by = "Segment")
+
+  out <- out %>%
+    mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
+                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction))
+    ) %>%
+    select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Nat, Emisja)
+
+  return(out)
+
+}
