feaf8e3d16615f7d6fb3ede09ecf3156c6ddf0b6
krzys <krzysztof.zaczyk7@gmail.com>
2020-11-23 00:26:22 +0100

    aa

--- R/fun_pack.R
+++ R/fun_pack.R
@@ -20,26 +20,27 @@ fun_pack <- function(dane = input,
 
   # Zabezpieczenia argumentów funkcji i danych wejscioweych.
 
-   input <- data.frame(Nat = rnorm(50, mean = 100, sd = 50),
+  input <- data.frame(Nat = rnorm(50, mean = 100, sd = 50),
                       Segment = sample(c("Mini", "Small", "Medium", "Large-SUV-Executive"),
                                        size = 50, replace = T),
                       paliwo = "Petrol",
                       technologia = "GDI")
 
-   input$Segment <- as.character(input$Segment)
+  input$Segment <- as.character(input$Segment)
 
-    out <- wskazniki
-    out <- dplyr::filter(Category == kategoria)
-    out <- dplyr::filter(Euro.Standard == euro)
-    out <- dplyr::filter(Pollutant == substancja)
-    out <- dplyr::filter(Mode == mode)
+  out <- wskazniki %>%
+    filter(Category %in% kategoria) %>%
+    filter(Euro.Standard %in% euro) %>%
+    filter(Pollutant %in% substancja) %>%
+    filter(Mode %in% mode)
 
-  out <- dplyr::inner_join(x = out, y = input, by = "Segment")
+  out <- inner_join(x = out, y = input, by = "Segment")
 
-  out <- dplyr::mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
-                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction)))
-  out <- dplyr::select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Nat, Emisja)
+  out <- out %>%
+    mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
+                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction))
+    ) %>%
+    select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Nat, Emisja)
 
   return(out)
-
 }
